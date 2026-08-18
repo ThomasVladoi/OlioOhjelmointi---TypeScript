b0feeb1fff7755d9cfdfaeea0e044c3a282ec54f
Thomas Vladoi <thomas.vladoi@kukkula.local>
2026-08-18 14:07:22 +0300

    .

--- src/shapes.ts
+++ src/shapes.ts
@@ -1,62 +1,74 @@
-export class Point {
-    x: number;
-    y: number;
+export class Point { 
+    x: number; 
+    y: number; 
+ 
+    constructor(x: number, y: number) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+} 
+ 
+export class Size { 
+    width: number; 
+    height: number; 
+ 
+    constructor(width: number, height: number) { 
+        this.width = width; 
+        this.height = height; 
+    } 
+} 
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+export class Shape {
+    style: string;
+
+    constructor(style: string) {
+        this.style = style;
     }
 }
+ 
+export class Rectangle extends Shape { 
+    location: Point; 
+    size: Size; 
+ 
+    constructor(x: number, y: number, width: number, height: number) { 
+        super("blue");
 
-export class Size {
-    width: number;
-    height: number;
+        this.location = new Point(x, y); 
+        this.size = new Size(width, height); 
+    } 
+ 
+    draw(ctx: CanvasRenderingContext2D) { 
+        ctx.fillStyle = this.style; 
+        ctx.fillRect( 
+            this.location.x, 
+            this.location.y, 
+            this.size.width, 
+            this.size.height 
+        ); 
+    } 
+} 
+ 
+export class Circle extends Shape { 
+    center: Point; 
+    radius: number; 
+ 
+    constructor(x: number, y: number, radius: number) { 
+        super("red");
 
-    constructor(width: number, height: number) {
-        this.width = width;
-        this.height = height;
-    }
-}
-
-export class Rectangle {
-    location: Point;
-    size: Size;
-
-    constructor(x: number, y: number, width: number, height: number) {
-        this.location = new Point(x, y);
-        this.size = new Size(width, height);
-    }
-
-    draw(ctx: CanvasRenderingContext2D) {
-        ctx.fillStyle = "blue";
-        ctx.fillRect(
-            this.location.x,
-            this.location.y,
-            this.size.width,
-            this.size.height
-        );
-    }
-}
-
-export class Circle {
-    center: Point;
-    radius: number;
-
-    constructor(x: number, y: number, radius: number) {
-        this.center = new Point(x, y);
-        this.radius = radius;
-    }
-
-    draw(ctx: CanvasRenderingContext2D) {
-        ctx.beginPath();
-        ctx.arc(
-            this.center.x,
-            this.center.y,
-            this.radius,
-            0,
-            2 * Math.PI
-        );
-        ctx.fillStyle = "red";
-        ctx.fill();
-    }
+        this.center = new Point(x, y); 
+        this.radius = radius; 
+    } 
+ 
+    draw(ctx: CanvasRenderingContext2D) { 
+        ctx.beginPath(); 
+        ctx.arc( 
+            this.center.x, 
+            this.center.y, 
+            this.radius, 
+            0, 
+            2 * Math.PI 
+        ); 
+        ctx.fillStyle = this.style; 
+        ctx.fill(); 
+    } 
 }
